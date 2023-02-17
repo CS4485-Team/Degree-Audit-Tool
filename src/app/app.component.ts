e87df5049c814756b4f24c5bf7f5ed43de042a61
Collin Matz <collin.matz.a@gmail.com>
2023-02-17 15:22:41 -0600

    merged home-page to front-end branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Degree-Audit-Tool';
 }
