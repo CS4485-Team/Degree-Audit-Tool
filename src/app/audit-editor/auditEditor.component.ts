596b060831cd3f2e44630b7a9ecca5692d190ff8
collinmatz <63628443+collinmatz@users.noreply.github.com>
2023-02-24 11:03:45 -0600

    updated audit-editor page and added navbar

--- src/app/audit-editor/auditEditor.component.ts
+++ src/app/audit-editor/auditEditor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import auditReportConfigs from 'auditreportConfig.json';
+import Handsontable from 'handsontable';
 
 /*
     loads data to prepolulate the table. This should only be called if the table
@@ -9,7 +10,7 @@ const loadPrepopulationData = () : any[] => {
     let data: any[] = [];
     data.push(['Course Title', 'Course Number', 'UTD Semester', 'Transfer', 'Grade']);
     for (let i = 0; i < auditReportConfigs.courseCount.numCoreCourses; ++i) {
-        data.push(['', '', '', '', ''])
+        data.push(['', '', '', '', '']);
     }
 
     return data;
@@ -24,4 +25,8 @@ const loadPrepopulationData = () : any[] => {
 export class AuditEditorComponent {
     // data to pre-populate audit reports
     preloadData: any[] = loadPrepopulationData();
+    title_1: any[] = [
+        ['CORE COURSES         (15 CREDIT HOURS)       3.19 Grade Point Average Required']
+    ];
+    
 }
