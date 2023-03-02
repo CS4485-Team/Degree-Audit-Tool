a138f6100efb26ff46c7e186bd3ff72078e5547b
devon-caron <devon.caron.p@gmail.com>
2023-03-01 22:04:15 -0600

    Added Java source files to branch

---
+++ Java Files/src/IntelSysPDF.java
@@ -0,0 +1,182 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+ 
+public class IntelSysPDF extends DefaultPDF {
+ 
+    // This function puts everything in place
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        
+        // Top of document. Did not PDFBuilder.make a function in order to customize with precision. Will do so later when expandability needs to be implemented
+        PdfPTable introTable = new PdfPTable(1);     
+        introTable.setWidthPercentage(PDFBuilder.WIDTH_PERCENTAGE);
+        PdfPCell cell = new PdfPCell(new Phrase(10f,   "                          DEGREE PLAN               ____________"
+                                                   + "\n                 UNIVERSITY OF TEXAS AT DALLAS      ____________"
+                                                   + "\n                  MASTERS OF COMPUTER SCIENCE       ____________"
+                                                   + "\n                                                    ____________"
+                                                   + "\n                      INTELLIGENT SYSTEMS                       "
+                                                   + "\n                                                               "
+                                                   + "\nName:                                      FT:                 "
+                                                   + "\nID:                                    Thesis:                 "
+                                                   + "\nApplied In:                                                    "
+                                                   + "\n                         Anticipated Graduation:               "
+                                                   + "\n                                                               "
+                                                   , PDFBuilder.FONT_TWELVE)); 
+        introTable.addCell(cell);
+        document.add(introTable);
+ 
+        PdfPTable headers = PDFBuilder.makeDefaultTable(PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, "           Course Title              ",
+                "Course Num ", "  UTD Sem  ", " Transfer  ", "   Grade   ");
+        document.add(headers);
+ 
+        PdfPTable reqCoursesHeader = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_ELEVEN, PDFBuilder.BEIGE, "CORE COURSES            (15 Credit Hours)            3.19 GPA Required");
+        document.add(reqCoursesHeader);
+ 
+        
+        // Each list here represents values in each column. Every section will need these values and a for loop will be used to insert them into the pdf.
+        Font rcTitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE,
+                PDFBuilder.FONT_SEVENPTFIVE };
+        Font rcOtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String rcTitles[] = { "Natural Language Processing", "Design and Analysis of Computer Algorithms",
+                "Artificial Intelligence", "Machine Learning", PDFBuilder.EMPTY };
+        String rcCourseNums[] = { "   CS6320  ", "   CS6363  ", "   CS6364  ", "   CS6375  ", PDFBuilder.EMPTY };
+        String rcSems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String rcTransfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String rcGrade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 5; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(rcTitleSize[i], rcOtherSize[i], rcTitles[i], rcCourseNums[i], rcSems[i],
+                    rcTransfer[i], rcGrade[i]);
+            document.add(temp);
+        }
+ 
+        // ============================================CORE
+        // COURSES=========================================================================================
+        PdfPTable coreCoursesHeader = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_ELEVEN, PDFBuilder.BEIGE, "                  One of the following 5 Core Courses                   ");
+        document.add(coreCoursesHeader);
+ 
+ 
+        Font ccTitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE };
+        Font ccOtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String ccTitles[] = { "Database Design", "Advanced Operating Systems", PDFBuilder.EMPTY };
+        String ccCourseNums[] = { "   CS6347  ", "   CS6378  ", PDFBuilder.EMPTY };
+        String ccSems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ccTransfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ccGrade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 3; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(ccTitleSize[i], ccOtherSize[i], ccTitles[i], ccCourseNums[i], ccSems[i],
+                    ccTransfer[i], ccGrade[i]);
+            document.add(temp);
+        }
+ 
+        // ============================================ELECTIVE COURSES=========================================================================================
+        PdfPTable elecCoursesHeader = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_TEN, PDFBuilder.BEIGE, "5 APPROVED 6000 COURSES         (15* Credit Hours)         3.0 GPA Required");
+        document.add(elecCoursesHeader);
+ 
+        // Each list here represents values in each column. Every section will need these values and a for loop will be used to insert them into the pdf.
+        Font ecTitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE,
+                PDFBuilder.FONT_SEVENPTFIVE };
+        Font ecOtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String ecTitles[] = { "1.", "2.", "3.", "4.", "5." };
+        String ecCourseNums[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ecSems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ecTransfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ecGrade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 5; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(ecTitleSize[i], ecOtherSize[i], ecTitles[i], ecCourseNums[i], ecSems[i],
+                    ecTransfer[i], ecGrade[i]);
+            document.add(temp);
+        }
+        
+        PdfPTable elecCoursesHeader2 = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_ELEVEN, PDFBuilder.BEIGE, "                Additional Electives (3 Credit Hours Min.)                 ");
+        document.add(elecCoursesHeader2);
+ 
+        Font ec2TitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE };
+        Font ec2OtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String ec2Titles[] = { "6.", "7.", "8." };
+        String ec2CourseNums[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ec2Sems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ec2Transfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String ec2Grade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 3; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(ec2TitleSize[i], ec2OtherSize[i], ec2Titles[i], ec2CourseNums[i], ec2Sems[i],
+                    ec2Transfer[i], ec2Grade[i]);
+            document.add(temp);
+        }
+        
+        // OTHER NON CS REQ'D COURSES
+        
+        PdfPTable otherReqCoursesHeader = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_ELEVEN, PDFBuilder.BEIGE, "                            Other Requirements                             ");
+        document.add(otherReqCoursesHeader);
+ 
+        Font orcTitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE };
+        Font orcOtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String orcTitles[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String orcCourseNums[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String orcSems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String orcTransfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String orcGrade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 2; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(orcTitleSize[i], orcOtherSize[i], orcTitles[i], orcCourseNums[i], orcSems[i],
+                    orcTransfer[i], orcGrade[i]);
+            document.add(temp);
+        }
+        
+        
+ 
+        // ===================================================PREREQS=============================================================================
+ 
+        PdfPTable prereqHeaders = PDFBuilder.makeDefaultHeader(PDFBuilder.FONT_NINE, PDFBuilder.BEIGE, "Admission Prerequisites               Course Num    UTD Sem      Waiver       Grade   ");
+        document.add(prereqHeaders);
+        
+        Font prcTitleSize[] = { PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE,
+                PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE, PDFBuilder.FONT_SEVENPTFIVE };
+        Font prcOtherSize[] = { PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE, PDFBuilder.FONT_NINE };
+        String prcTitles[] = { "Computer Science I", "Computer Science II",
+                "Discrete Structures", "Algorithm Analysis & Data Structures", "Operating Systems Concepts", PDFBuilder.EMPTY };
+        String prcCourseNums[] = { "   CS5303   ", "   CS5330    ", "   CS5333   ", "   CS5343   ", "   CS5348   ", PDFBuilder.EMPTY };
+        String prcSems[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String prcTransfer[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+        String prcGrade[] = { PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY, PDFBuilder.EMPTY };
+ 
+        for (int i = 0; i < 6; i++) {
+            PdfPTable temp = PDFBuilder.makeDefaultTable(prcTitleSize[i], prcOtherSize[i], prcTitles[i], prcCourseNums[i], prcSems[i],
+                prcTransfer[i], prcGrade[i]);
+            document.add(temp);
+        }
+ 
+ 
+ 
+        // Signature page also highly customizable.
+        
+        PdfPTable sigTable = new PdfPTable(1); 
+        sigTable.setWidthPercentage(PDFBuilder.WIDTH_PERCENTAGE);
+        cell = new PdfPCell(new Phrase(10f,            "*May include any 6000/7000 CS Course Without Prior Permission"
+                                                   + "\n                                                             "
+                                                   + "\n                                                             "
+                                                   + "\n                                                             "
+                                                   + "\n                                                             "
+                                                   + "\nAdvisor:__________________________          Date Submitted:___________"
+                                                   + "\n                                                             "
+                                                   , FontFactory.getFont(FontFactory.COURIER, 11f, Font.BOLD)));
+        sigTable.addCell(cell);
+        document.add(sigTable);
+
+        document.close();
+    }
+}
---
+++ Java Files/src/PDFBuilder.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+ 
+public class PDFBuilder {
+    
+    public static final int WIDTH_PERCENTAGE = 90;
+    public static final BaseColor BEIGE = new BaseColor(253, 200, 155);
+    public static final BaseColor CYAN = new BaseColor(200, 255, 255);
+    public static final String DEST = "src/table.pdf";
+    public static final Font FONT_TWELVE = FontFactory.getFont(FontFactory.COURIER, 12f, Font.BOLD);
+    public static final Font FONT_ELEVEN = FontFactory.getFont(FontFactory.COURIER, 11f, Font.BOLD);
+    public static final Font FONT_TEN = FontFactory.getFont(FontFactory.COURIER, 10f, Font.BOLD);
+    public static final Font FONT_NINE = FontFactory.getFont(FontFactory.COURIER, 9f, Font.BOLD);
+    public static final Font FONT_SEVENPTFIVE = FontFactory.getFont(FontFactory.COURIER, 7.5f, Font.BOLD);
+    public static final Font FONT_SIXPTEIGHT = FontFactory.getFont(FontFactory.COURIER, 6.8f, Font.BOLD);
+    public static final String EMPTY = "           ";
+ 
+    public static void main(String[] args) throws IOException, DocumentException {
+    	Scanner input = new Scanner(System.in);
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        DefaultPDF pdf = null;
+        System.out.println("1 for datasci, 2 for intelsys");
+        String next = input.next();
+        if (next.equals("1")) {
+        	pdf = new DataSciPDF();
+        } else if (next.equals("2")) {
+        	pdf = new IntelSysPDF();
+        } else if (next.equals("3")) {
+        	pdf = new SysTrackPDF();
+        }
+        
+        pdf.createPdf(DEST);
+        input.close();
+        
+    }
+ 
+    /** Makes a title header that is able to be colored.
+     * 
+     * @param fontSize
+     * @param color
+     * @param text
+     * @return
+     */
+    public static PdfPTable makeDefaultHeader(Font fontSize, BaseColor color, String text) {
+        PdfPTable header = new PdfPTable(1);
+        header.setWidthPercentage(WIDTH_PERCENTAGE);
+        
+        PdfPCell cell = new PdfPCell(new Phrase(10f, text, fontSize));
+        System.out.println(color);
+        cell.setBackgroundColor(color);
+        header.addCell(cell);
+        
+        return header;
+    }
+    
+    /** Makes a default table containing 1 row, containing course title, number, semester, transfer/waiver, and grade.
+     * 
+     * @param titleFontSize
+     * @param otherFontSize
+     * @param courseTitle
+     * @param courseNum
+     * @param sem
+     * @param tsf
+     * @param grade
+     * @return
+     * @throws DocumentException
+     */
+    public static PdfPTable makeDefaultTable(Font titleFontSize, Font otherFontSize, String courseTitle,
+            String courseNum, String sem, String tsf, String grade) throws DocumentException {
+        PdfPTable table = new PdfPTable(5);
+        table.setWidthPercentage(WIDTH_PERCENTAGE);
+        table.setWidths(new int[] { 3, 1, 1, 1, 1 });
+        PdfPCell cell;
+ 
+        cell = new PdfPCell(new Phrase(5f, courseTitle, titleFontSize));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(5f, courseNum, otherFontSize));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(5f, sem, otherFontSize));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(5f, tsf, otherFontSize));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(5f, grade, otherFontSize));
+        table.addCell(cell);
+ 
+        return table;
+ 
+    }
+}
