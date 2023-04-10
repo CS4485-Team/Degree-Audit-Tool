6641d302e619364c13904878da7112bb9f8d6581
devon-caron <devon.caron.p@gmail.com>
2023-04-09 22:24:25 -0500

    build path issues

--- Java Files/src/AuditPDFBuilder.java
+++ Java Files/src/AuditPDFBuilder.java
@@ -1,18 +1,21 @@
 import com.itextpdf.text.*;
 
 public class AuditPDFBuilder {
+	public static void main(String[] args) {
+		sysout
+	}
 	public void createAudRep(String dest) {
 		Phrase toInsert = new Phrase(10f,   "                          DEGREE PLAN               ____________"
-                + "\n                 UNIVERSITY OF TEXAS AT DALLAS      ____________"
-                + "\n                  MASTERS OF COMPUTER SCIENCE       ____________"
-                + "\n                                                    ____________"
-                + "\n                         SYSTEMS TRACK                         "
-                + "\n                                                               "
-                + "\nName:                                      FT:                 "
-                + "\nID:                                    Thesis:                 "
-                + "\nApplied In:                                                    "
-                + "\n                         Anticipated Graduation:               "
-                + "\n                                                               "
-                , PDFBuilder.FONT_TWELVE)
+						                + "\n                 UNIVERSITY OF TEXAS AT DALLAS      ____________"
+						                + "\n                  MASTERS OF COMPUTER SCIENCE       ____________"
+						                + "\n                                                    ____________"
+						                + "\n                         SYSTEMS TRACK                          "
+						                + "\n                                                                "
+						                + "\nName:                                      FT:                  "
+						                + "\nID:                                    Thesis:                  "
+						                + "\nApplied In:                                                     "
+						                + "\n                         Anticipated Graduation:                "
+						                + "\n                                                                "
+                , PDFBuilder.FONT_TWELVE);
 	}
 }
