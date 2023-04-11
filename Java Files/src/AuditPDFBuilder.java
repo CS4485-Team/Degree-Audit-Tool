947557b97b02fb2160947be7e7ce77becba69a0e
Devon Caron <devon.caron.p@gmail.com>
2023-04-10 20:50:52 -0500

    More audit changes

--- Java Files/src/AuditPDFBuilder.java
+++ Java Files/src/AuditPDFBuilder.java
@@ -8,9 +8,10 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class AuditPDFBuilder {
 	public static void main(String[] args) {
-		createAudRep("./src/audtable.pdf");
+		String[] temp = new String[8];
+		createAudRep("./src/audtable.pdf", temp, 0, 0, 0);
 	}
-	public static void createAudRep(String dest) {
+	public static void createAudRep(String dest, String[] classes, int coreEndIndex, int elecEndIndex, int preEndIndex) {
 		try {
 	        Document document = new Document();
 	        PdfWriter.getInstance(document, new FileOutputStream(dest));
@@ -27,7 +28,9 @@ public class AuditPDFBuilder {
 	        String line4 = String.format("\nCORE GPA: %-57.3f", 3.755);
 	        String line5 = String.format("\nELECTIVE GPA: %-53.3f", 3.755);
 	        String line6 = String.format("\nCOMBINED GPA: %-53.3f", 3.755);
-	        String line7 = String.format("\nCOMBINED GPA: %-53.3f", 3.755);
+	        String line7 = "CORE ELECTIVES: ";
+
+			for (int i = 0; i < coreEndIndex; i++)
 	        
 	        
 	        String toInsert = "\n\n" + line1 + line2 + line3 + "\n" + line4 + line5 + line6 + "\n" + line7;
