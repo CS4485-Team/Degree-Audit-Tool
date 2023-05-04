c77f1b8f66fcadea3aeda9612f6190b2aa28fe13
Fafnir3 <jhsieh412@gmail.com>
2023-05-04 14:17:05 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ import java.io.FileWriter;
 
 public class Main {
     public static void main(String args[]) throws Exception {
+        //Accepts first argument when Jar file is ran
         String inputFilePath = args[0];
         File inputFile = new File(inputFilePath);
         transcriptParser transcript = new transcriptParser(inputFile);
+        //Calls getStudent command to get Student information
         StudentParser curr = transcript.getStudent();
-
+        //Calls Get thesis course to get courses
         curr.getThesisCourses();
-
+        //Calls printStudentInformation which generates a CSV with student info and all class description
         curr.printStudentInformation();
 
     }
