d9c582a1fca235b585ac037f717a0202064cdc89
Collin Matz <collin.matz.a@gmail.com>
2023-05-06 10:19:06 -0500

    fixing up start-select page

--- src/app/startSelect-page/startSelect.component.ts
+++ src/app/startSelect-page/startSelect.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare const electron: any;
+
 @Component({
   selector: 'startSelect',
   templateUrl: './startSelect.component.html',
@@ -8,50 +10,76 @@ import { Router } from '@angular/router';
 })
 
 export class StartSelectComponent {
-    inputNode: any;
+    inputFileName: string | null = "";
+
     constructor(private router: Router) {}
 
-    uploadTranscriptFile() {
-      let container: any = document.getElementById("continueButton");
-      this.inputNode = document.createElement("input");
-      this.inputNode.type = "file";
-      this.inputNode.click();
-    }
-
-    uploadStudentObjectFile() {
-
-    }
-
-    continue() {
-      let r = this.router;
-      let promise = new Promise(function(resolve, reject) {
-        let files: any = document.getElementById("transcriptInput"); // FileList object
-        // console.log(files.files[0]);
-
-        // use the 1st file from the list
-        let f = files.files[0];
-        
-        let reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e: any) {
-              resolve(e.target.result);
-            };
-        })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsText(f);
-      })
-      
-      promise.then(
-        function(value) {
-          r.navigateByUrl('/degreePlan', {state: {preload: value}})
-        }
-      )
-    }
-
+    // allow the user to continue to the degree plan creation page without
+    //  uploading one of the prepopulation files
     continueScratch() {
       this.router.navigateByUrl("/degreePlan", {state: {preload: []}})
+    }
+
+    // upload a student transcript file to prepopulate the degree plan table.
+    //  only accepts pdf files as input
+    uploadTranscriptFile() {
+      const inputNode: HTMLInputElement = document.createElement("input");
+      inputNode.type = "file";
+      inputNode.accept = ".pdf";
+      inputNode.id = "fileInput";
+
+      const updateInputFileName = (event: any) => {
+        this.inputFileName = event.target.files[0].name;
+      }
+
+      inputNode.addEventListener("change", updateInputFileName, false);
+      inputNode.click();
+    }
+
+    // upload a student object file to prepopulate the degree plan table.
+    //  only accepts stdobj files as input
+    uploadStudentObjectFile() {
+      const inputNode: HTMLInputElement = document.createElement("input");
+      inputNode.type = "file";
+      inputNode.accept = ".stdobj";
+      inputNode.id = "fileInput";
+
+      const updateInputFileName = (event: any) => {
+        this.inputFileName = event.target.files[0].name;
+      }
+
+      inputNode.addEventListener("change", updateInputFileName, false);
+      inputNode.click();
+    }
+
+    // allow the user to continue to the next page after they have uploaded one of the required
+    //  files
+    continue() {
+      const router = this.router;
+
+      // const promise = new Promise(function(resolve, reject) {
+      //   let input: any = document.getElementById("fileInput");
+      //   console.log(input.files[0]);
+
+      //   let f = input.files[0];
+        
+      //   let reader = new FileReader();
+
+      //   reader.onload = (function(theFile) {
+      //       return function(e: any) {
+      //         resolve(e.target.result);
+      //       };
+      //   })(f);
+
+      //   // Read in the image file as a data URL.
+      //   reader.readAsText(f);
+      // })
+      
+      // promise.then(
+      //   function(value) {
+      //     router.navigateByUrl('/degreePlan', {state: {preload: value}})
+      //   }
+      // )
+
     }
   }
